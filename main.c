2ada72ab832fa40205195fc8d130587027f8c3e0
Mack Mason <mackmason16@gmail.com>
2020-09-03 01:23:30 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(void) {
+  char *celsiusString = readline("Enter temperature in celsius: ");
+  float celsiusFloat = atof(celsiusString);
+  float fahrenheit = (celsiusFloat * (9/5)+32);
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n",celsiusFloat,fahrenheit);
+  return 0;
+}
