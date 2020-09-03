3f3f4765d0b3a349c3efae2c2ab8c825070dd01b
Mack Mason <mackmason16@gmail.com>
2020-09-03 13:39:11 +0000

    lol

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+// Author: Mack Mason mjm8542@psu.edi
+// Collaborator: Brady Bender bmb6369@psu.edu
+// Collaborator: Addy Burchfield abb6060@psu.edu
+// Collaborator: Jacob Hallowell jph5997@psu.edu
+
+
 #include <stdio.h>
 #include <readline/readline.h>
 #include <stdlib.h>
